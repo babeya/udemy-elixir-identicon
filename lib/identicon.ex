332cf5e8ffb7f617f87c4d2c73ecabef09bba248
Arthur BABEY <git@ababey.com>
2019-03-15 12:49:10 +0100

    pick_color

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -13,9 +13,8 @@ defmodule Identicon do
     }
   end
 
-  def pick_color(image) do 
-    %Identicon.Image{hex: [r, g, b | _tail]} = image
-    [r, g, b]
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do 
+    %Identicon.Image{image | color: [r, g, b ]}
   end 
 
 end
